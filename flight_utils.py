875b43f7c617dfe1db3f3dbe07027070382379dc
91yael <91yael@gmail.com>
2024-08-07 13:32:56 +0300

    Added new file: kafka_producer_from_flights_api.py Updated requirements.txt with specific package versions Updated flight_utils.py with new functions and improvements

--- flight_utils.py
+++ flight_utils.py
@@ -11,6 +11,7 @@ class FlightUtils:
         self.rapidapi_host = os.getenv('RAPIDAPI_HOST', "skyscanner80.p.rapidapi.com")
         self.num_weekends_to_check = 1
 
+    # Get location ID
     def get_location_id(self, query, headers):
         response = requests.get(self.url_location, headers=headers, params={"query": query})
         if response.status_code != 200:
@@ -22,6 +23,7 @@ class FlightUtils:
         else:
             raise ValueError("Location ID not found in the response")
 
+    # Get flights
     def get_flights(self, from_id, to_id, depart_date, return_date, headers):
         querystring = {
             "fromId": from_id,
@@ -38,7 +40,34 @@ class FlightUtils:
         if response.status_code != 200:
             raise ValueError(f"Error: {response.status_code}, {response.text}")
         return response.json()
+    
+    #Clean flight data to remove empty entries
+    def clean_flight_data(self, flight_data):
+        cleaned_data = []
+        for flight in flight_data:
+            if isinstance(flight, dict) and all(isinstance(v, (str, int, float, list, dict)) for v in flight.values()):
+                cleaned_data.append({k: (v if v != {} else None) for k, v in flight.items()})
+        return cleaned_data
+    
+    #Extract top cheapest flights
+    def get_top_cheapest_flights(self, flights_data, top_cheapest_flights):
+        flights = []
+        itineraries = flights_data.get("data", {}).get("itineraries", [])
+    
+        for itinerary in itineraries:
+            price_raw = itinerary.get("price", {}).get("raw", float('inf'))
+            flights.append({
+                "id": itinerary.get("id"),
+                "price_raw": price_raw,
+                "itinerary": itinerary
+            })
+    
+        # Sort flights by price
+        flights_sorted = sorted(flights, key=lambda x: x["price_raw"])
+        return flights_sorted[:top_cheapest_flights]
 
+    
+    # Get next weekends
     def get_next_weekends(self, num_weekends=None):
         if num_weekends is None:
             num_weekends = self.num_weekends_to_check
@@ -49,7 +78,21 @@ class FlightUtils:
             end_of_weekend = start_of_weekend + timedelta(2)  # Saturday
             weekends.append((start_of_weekend.strftime("%Y-%m-%d"), end_of_weekend.strftime("%Y-%m-%d")))
         return weekends
-
+    
+    # Get next workdays
+    def get_next_workdays(self, num_weeks=None):
+        if num_weeks is None:
+            num_weeks = self.num_workdays_to_check
+        today = datetime.now()
+        workdays = []
+        for i in range(num_weeks):
+            start_of_week = today + timedelta((6 - today.weekday() + 7) % 7 + i * 7)  # Next Sunday
+            for j in range(5):  # Sunday through Thursday
+                workday = start_of_week + timedelta(days=j)
+                workdays.append(workday.strftime("%Y-%m-%d"))
+        return workdays
+    
+    # Load destinations
     def load_destinations(self, file_path):
         with open(file_path, 'r') as file:
             return json.load(file)
---
+++ kafka_producer_from_flights_api.py
@@ -0,0 +1,103 @@
+import os
+import json
+from dotenv import load_dotenv
+from kafka import KafkaProducer
+from flight_utils import FlightUtils
+
+"""Initialize parameters"""
+
+#The number of top cheapest flights to retrieve.
+num_cheapest_flights=3
+
+#The number of weekends to retrieve.
+next_weekends=2
+
+#The path of destinations file.
+destination_path = 'destinations.json'
+
+""""""
+
+#Send data to Kafka topic
+def send_to_kafka(producer, topic, data):
+    print(f"Sending {len(data)} records to Kafka topic '{topic}'...")
+    print(f"data: {data}")
+    for record in data:
+        producer.send(topic, json.dumps(record).encode('utf-8'))
+
+def main():
+    # Load environment variables from .env file
+    load_dotenv()
+    
+    api_key = os.getenv('RAPIDAPI_KEY')
+    kafka_broker = os.getenv('KAFKA_BROKER')
+    kafka_topic = os.getenv('KAFKA_TOPIC_FLIGHTS_3')
+
+    if not api_key or not kafka_broker or not kafka_topic:
+        raise ValueError("API key, Kafka broker, or topic not found. Make sure they are set in the .env file.")
+    
+    # Initialize FlightUtils
+    flight_utils = FlightUtils()
+
+    headers = {
+        "x-rapidapi-key": api_key,
+        "x-rapidapi-host": flight_utils.rapidapi_host
+    }
+
+    # Kafka producer setup
+    producer = KafkaProducer(
+        bootstrap_servers='localhost:29092',
+        retries=5,
+        request_timeout_ms=30000,  
+        batch_size=16384,  
+        linger_ms=5000, 
+        acks='all'  
+    )
+
+    # Load destinations
+    destinations = flight_utils.load_destinations(destination_path)
+    # Get next weekends
+    weekends = flight_utils.get_next_weekends(next_weekends)
+
+    for destination in destinations:
+        from_city = destination.get("from")
+        to_city = destination.get("to")
+        if not from_city or not to_city:
+            print("Missing 'from' or 'to' key in destination.")
+            continue
+        
+        for depart_date, return_date in weekends:
+            try:
+                print(f"Fetching location ID for {from_city} and {to_city}...")
+                from_id = flight_utils.get_location_id(from_city, headers)
+                to_id = flight_utils.get_location_id(to_city, headers)
+                
+                print(f"Fetching flights from {from_id} to {to_id} for dates {depart_date} to {return_date}...")
+                flights_data = flight_utils.get_flights(from_id, to_id, depart_date, return_date, headers)
+                
+                # Get top cheapest flights
+                top_cheapest_flights = flight_utils.get_top_cheapest_flights(flights_data,num_cheapest_flights)
+
+                # Prepare messages
+                messages = []
+                for flight in top_cheapest_flights:
+                    messages.append({
+                        "from_city": from_city,
+                        "to_city": to_city,
+                        "depart_date": depart_date,
+                        "return_date": return_date,
+                        "price_raw": flight["price_raw"],
+                        "itinerary": flight["itinerary"] 
+                    })
+                print(f"messages: {messages}")
+                print(f"producer: {producer}, kafka_topic: {kafka_topic}")
+                send_to_kafka(producer, kafka_topic, messages)
+
+            except Exception as e:
+                print(f"Error processing flight data: {str(e)}")
+
+    # Ensure all messages are sent
+    producer.flush()
+    producer.close()
+
+if __name__ == "__main__":
+    main()
